93e30749d05125cef52e806bfb65db1f963c2fcf
edward <edwardlfish@gmail.com>
2024-12-15 19:27:17 +0200

    Use HashMap in CompanyImpl

--- src/telran/employee/dao/CompanyImpl.java
+++ src/telran/employee/dao/CompanyImpl.java
@@ -3,19 +3,16 @@ package telran.employee.dao;
 import telran.employee.model.Employee;
 import telran.employee.model.SalesManager;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.function.Predicate;
 
 public class CompanyImpl implements Company {
-    private Set<Employee> employees;
+    private Map<Integer, Employee> employees;
     private int capacity;
 
     public CompanyImpl(int capacity) {
         this.capacity = capacity;
-        employees = new HashSet<>();
+        employees = new HashMap<>();
     }
 
     // O(1)
@@ -24,26 +21,19 @@ public class CompanyImpl implements Company {
         if (employee == null || capacity == employees.size()) {
             return false;
         }
-        return employees.add(employee);
+        return employees.putIfAbsent(employee.getId(), employee) == null;
     }
 
-    // O(n)
+    // O(1)
     @Override
     public Employee removeEmployee(int id) {
-        Employee victim = findEmployee(id);
-        employees.remove(victim);
-        return victim;
+        return employees.remove(id);
     }
 
-    // O(n)
+    // O(1)
     @Override
     public Employee findEmployee(int id) {
-        for (Employee employee : employees) {
-            if (employee.getId() == id) {
-                return employee;
-            }
-        }
-        return null;
+        return employees.get(id);
     }
 
     // O(1)
@@ -56,7 +46,7 @@ public class CompanyImpl implements Company {
     @Override
     public double totalSalary() {
         double sum = 0;
-        for (Employee employee : employees) {
+        for (Employee employee : employees.values()) {
             sum += employee.calcSalary();
         }
         return sum;
@@ -66,7 +56,7 @@ public class CompanyImpl implements Company {
     @Override
     public double totalSales() {
         double sum = 0;
-        for (Employee employee : employees) {
+        for (Employee employee : employees.values()) {
             if (employee instanceof SalesManager sm) {
                 sum += sm.getSalesValue();
             }
@@ -78,7 +68,7 @@ public class CompanyImpl implements Company {
     @Override
     public void printEmployees() {
         System.out.println("=== " + COUNTRY + " ===");
-        for (Employee employee : employees) {
+        for (Employee employee : employees.values()) {
             System.out.println(employee);
         }
         System.out.println("=======================");
@@ -98,7 +88,7 @@ public class CompanyImpl implements Company {
 
     private Employee[] findEmployeesByPredicate(Predicate<Employee> predicate) {
         List<Employee> res = new ArrayList<>();
-        for (Employee employee : employees) {
+        for (Employee employee : employees.values()) {
             if (predicate.test(employee)) {
                 res.add(employee);
             }
